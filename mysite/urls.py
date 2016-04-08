fa31467d53bfc93a9222d8a8473f7ad0ad285c81
ravil <raul_mahmutov@mail.ru>
2016-04-08 11:33:54 +0400

    Added imagest, just for testing

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url, patterns
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'', include('blog.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
